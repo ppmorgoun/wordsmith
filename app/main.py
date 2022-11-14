fe7fcdefff68d5c05256e2a01d783acba0f724a9
ppmorgoun <ppmorgoun@gmail.com>
2022-11-14 17:29:03 +1300

    cleaned up some old files, fixed word definition styling, added text box to word definition popup

--- app/main.py
+++ app/main.py
@@ -8,9 +8,9 @@ from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 from kivy.properties import ObjectProperty, StringProperty, ListProperty
 from kivy.clock import Clock
 from PyDictionary import PyDictionary
-import mysql.connector
 import sqlite3
-
+import os
+__version__ = "0.1"
 kivy.require('2.0.0')
 
 class WordBox(Popup):
@@ -30,11 +30,26 @@ class EnterWord(Screen):
     dictionary=PyDictionary()
 
     def search_word_meaning(self):
+        # searches for the meaning of the word entered in the text input
         word_stripped = self.new_word.text.strip()
-        meaning = str(self.dictionary.meaning(word_stripped))
-        self.word_meaning = meaning
+        meaning = self.dictionary.meaning(word_stripped)
+        if meaning is not None:
+            meaning_cleaned = self.unwrap_definition(meaning)
+            self.word_meaning = meaning_cleaned
+        else:
+            self.word_meaning = 'Word not found'
+
+    def unwrap_definition(self, meaning):
+        meaning_clean = ''
+        for i, j in meaning.items():
+            sub_meaning = '{type}: \n'.format(type = i)
+            for m in j:
+                sub_meaning += m.capitalize() + ';\n'
+            meaning_clean += sub_meaning + '\n\n'
+        return meaning_clean
 
     def submit_new_word(self):
+        # writes a word, it's meaning, and the default bucket number to the database
         word_stripped = self.new_word.text.strip().capitalize()
         if word_stripped != '':
             conn = sqlite3.connect('data/wordbank.db')
@@ -57,13 +72,15 @@ class EnterWord(Screen):
 
 class HomePageScreen(Screen):
     def word_box(self, word, definition):
+        # this is a popup that shows the word and definition
         p = WordBox()
         p.word = word
         p.definition = definition
         p.open()
-        print("{} was pressed".format(word))
+        print("{}'s definition was shown".format(word))
 
     def remove_word_box(self, word):
+        # this is a popup that asks if you want to remove the word
         p = RemoveWordBox()
         p.word = word
         p.open()
@@ -73,10 +90,6 @@ class HomePageScreen(Screen):
 
 class HomePage(RecycleView):
 
-    
-    word_bank = ListProperty('')
-    word = StringProperty()
-    #app = App.get_running_app()
     def __init__(self, **kwargs):
         super(HomePage, self).__init__(**kwargs)
         conn = sqlite3.connect('data/wordbank.db')
@@ -87,15 +100,8 @@ class HomePage(RecycleView):
 
         self.data= [{'text': str(word), 'id': str(definition)} for word, definition in word_row]  
 
-    """ 
-    def __init__(self, **kwargs):
-        super(HomePage, self).__init__(**kwargs)
-        Clock.schedule_once(self.after_init)
-
-    def after_init(self, dt):
-        self.data = [{'text': "Wordsmith", 'id': "A skilled user of words"}] """
-
     def update_word(self):
+        # a popup called from the recycleviewrow that updates the database with a word, definition, and bucket
         conn = sqlite3.connect('data/wordbank.db')
         c = conn.cursor()
         c.execute("SELECT word, definition FROM words")
@@ -105,12 +111,8 @@ class HomePage(RecycleView):
         self.data= [{'text': str(word), 'id': str(definition)} for word, definition in word_row]   
         print('updated home page with words from db')
 
-        
-
     def remove_word(self, word):
-        """ del word_bank[word]
-        self.update_word() """
-
+        # a popup called by the recycleviewrow that removes a word from the database
         conn = sqlite3.connect('data/wordbank.db')
         c = conn.cursor()
         sql_command = "DELETE FROM words WHERE word = ?"
@@ -124,32 +126,31 @@ class HomePage(RecycleView):
     
 
 class Manager(ScreenManager):
-    """ global word_bank
-    word_bank = {}
-    word_bank["Wordsmith"] = "A skilled user of words"
- """
     homepagescreen = ObjectProperty(None)
     enterword = ObjectProperty(None)
 
 class WordSmith(App):
     def build(self):
-        sm = Manager(transition=NoTransition())
-        conn = sqlite3.connect('data/wordbank.db')
-        c = conn.cursor()
-        c.execute("""CREATE TABLE if not exists words(
+        path = './data/wordbank.db'
+        if not os.path.exists(path): 
+            # create our database if it doesn't exist
+            # create a table for words
+            conn = sqlite3.connect('data/wordbank.db')
+            c = conn.cursor()
+            c.execute("""CREATE TABLE if not exists words(
 			word VARCHAR(50), definition VARCHAR(300), bucket INT, UNIQUE(word))
 		 """)
-        c.execute("SELECT * FROM words")
-        conn.commit()
+            conn.commit()
+            # insert "wordsmith" and it's definition into words table
+            c.execute("""INSERT INTO words (word, definition, bucket) VALUES ('Wordsmith', 'A skilled user of words', 1)""")
+            conn.commit()
+            conn.close()
 
-        conn.close()
-
-
+        sm = Manager(transition=NoTransition())
         return sm
 
 
 WordApp = WordSmith()
-
 
 if __name__ == '__main__':
     WordApp.run()
---
+++ app/readme.txt
@@ -0,0 +1,3 @@
+Building recycle view funtionality:
+https://stackoverflow.com/questions/53834602/kivy-selectable-recycleview-inside-screen-manager
+https://stackoverflow.com/questions/46296821/kivy-recycleview-as-an-alternative-to-listview-how-does-it-work
